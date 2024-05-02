0c42f03d69006c7994df1be6fbacc9073e7a17d8
Bahtiyar-Tulendybaev <119873603+Bahtiyar-Tulendybaev@users.noreply.github.com>
2024-05-02 19:08:09 +0600

    initial commit

---
+++ src/main/java/com/example/neo_auth_project/NeoAuthProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.neo_auth_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoAuthProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeoAuthProjectApplication.class, args);
+    }
+
+}
