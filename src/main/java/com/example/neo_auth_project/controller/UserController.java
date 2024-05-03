d965f9b5228db44a0e4c3366a0ab49d4b2ad7992
Bahtiyar-Tulendybaev <119873603+Bahtiyar-Tulendybaev@users.noreply.github.com>
2024-05-03 23:54:50 +0600

    run the project

--- src/main/java/com/example/neo_auth_project/controller/UserController.java
+++ src/main/java/com/example/neo_auth_project/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.example.neo_auth_project.controller;
 
-
 import com.example.neo_auth_project.dto.AuthenticationResponse;
 import com.example.neo_auth_project.dto.LoginRequest;
 import com.example.neo_auth_project.dto.UserDto;
@@ -14,52 +13,52 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
-        @Autowired
-        UserService userService;
+    @Autowired
+    UserService userService;
 
 
-        @GetMapping(value="/all")
-        public ResponseEntity<?> getAllCustomer() {
+    @GetMapping(value = "/all")
+    public ResponseEntity<?> getAllCustomer() {
 
-            return ResponseEntity.ok().body(userService.getAllCustomer());
-        }
-
-        @GetMapping("/{id}")
-        public ResponseEntity<?> getCustomer(@PathVariable("id") Long id){
-
-           return ResponseEntity.ok().body(userService.findById(id));
-        }
-
-        @DeleteMapping("/{id}")
-        public ResponseEntity<?> deleteUser(@PathVariable ("id") Long id){
-
-            return ResponseEntity.ok().body(userService.deleteById(id));
-
-        }
-
-@PostMapping("/add")
-public ResponseEntity<?> register(@RequestBody UserDto request){
-    try {
-        userService.register(request);
-    }catch (UserAlreadyExistsException e){
-        return ResponseEntity.badRequest().body(e.getMessage());
+        return ResponseEntity.ok().body(userService.getAllCustomer());
     }
-    return ResponseEntity.ok().build();
-}
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getCustomer(@PathVariable("id") Long id) {
+
+        return ResponseEntity.ok().body(userService.findById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable("id") Long id) {
+
+        return ResponseEntity.ok().body(userService.deleteById(id));
+
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody UserDto request) {
+        try {
+            userService.register(request);
+        } catch (UserAlreadyExistsException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+        return ResponseEntity.ok().build();
+    }
 
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequest request){
+    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
         AuthenticationResponse authenticationResponse;
         try {
             authenticationResponse = userService.login(request);
-        }catch (UserNotFoundException e){
+        } catch (UserNotFoundException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
         return ResponseEntity.ok(authenticationResponse);
     }
 
-    }
+}
 
 
 
