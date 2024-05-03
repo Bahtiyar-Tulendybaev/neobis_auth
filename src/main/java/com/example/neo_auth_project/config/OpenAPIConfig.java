43380a6f70ec7f2694a9bb087d9a13715754250f
Bahtiyar-Tulendybaev <119873603+Bahtiyar-Tulendybaev@users.noreply.github.com>
2024-05-04 01:38:24 +0600

    add some changes

--- src/main/java/com/example/neo_auth_project/config/OpenAPIConfig.java
+++ src/main/java/com/example/neo_auth_project/config/OpenAPIConfig.java
@@ -4,6 +4,7 @@ package com.example.neo_auth_project.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
         info = @Info(
@@ -14,7 +15,13 @@ import io.swagger.v3.oas.annotations.info.Info;
                 description = "OpenAPI documentation",
                 title = "OpenAPI specification - Bahtiyar",
                 version = "1.0"
-        )
+        ),
+        servers = {
+                @Server(
+                        description = "Railway App",
+                        url = "https://neobisauth-production.up.railway.app/"
+                )
+        }
 
 )
 public class OpenAPIConfig {
